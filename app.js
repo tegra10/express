2860e51d9aeab93590a10db3ca7577dea31045f7
tegra20 <tegkit30@gmail.com>
2024-05-09 23:22:58 +0100

    second commit

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
-console.log("ok");
-console.log("tres bien");
+const express = require("express");
+const port = 3000;
+
+const app = express();
+app.use('/post', require('./backend/post.routes'))
+app.listen(port, () => {
+    console.log(`le servereur est lancé sur le port ${port}`);
+});
