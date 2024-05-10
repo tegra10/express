3c2b808df3f2dd92f3cc45a7ba4934b8ea3a0e64
tegra20 <tegkit30@gmail.com>
2024-05-10 20:44:06 +0100

    sixth commit with errors of URI of mongoDB or json-server

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 const express = require("express");
+const dotenv = require("dotenv").config();
+const connectDB = require("./config/db.js");
 const port = 3000;
+connectDB();
 const app = express();
 
-//les middleware
-// app.use(express.json());
-// app.use(express.urleconded({ extended: false }));
-
+// Les middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-//les routes
-app.use("/post", require("./routes/post.routes"));
+// Les routes
+// Utilisez un proxy pour rediriger les requêtes vers json-server
+app.use("/post", (req, res) => {
+    const httpProxy = require("http-proxy");
+    const proxy = httpProxy.createProxyServer({});
+    proxy.web(req, res, { target: "http://localhost:3001" });
+});
+
 app.listen(port, () => {
-    console.log(`le servereur est lancé sur le port ${port}`);
+    console.log(`Le serveur est lancé sur le port ${port}`);
 });
